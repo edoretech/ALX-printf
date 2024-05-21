8b53c813a66f5f0634ee5b48ce096d8a79b9b76f
EMMANUEL AYOBAMI <135729444+SCCSMARTCODE@users.noreply.github.com>
2024-05-21 23:36:31 +0100

    Update main.h

--- main.h
+++ main.h
@@ -3,21 +3,6 @@
 #define UNNSED(x) (void)x
 #define BUFFER_SIZE 1024
 
-
-/*Standard Libraries*/
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-#include <stdarg.h>
-#include <stdlib.h>
-#include <limits.h>
-
-
-/*Defined functions*/
-int _printf(char *str, ...);
-int print_int(int num);
-int print_unsigned(unsigned int num);
-char *print_bin(int num);
-
+#include "printf.h"
 
 #endif
