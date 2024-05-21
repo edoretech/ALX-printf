9f1de5d3eb26042efd2ad1e08696b65c9b6d8f74
EMMANUEL AYOBAMI <135729444+SCCSMARTCODE@users.noreply.github.com>
2024-05-21 02:30:42 +0100

    Update data_type_printer.c

--- data_type_printer.c
+++ data_type_printer.c
@@ -6,24 +6,36 @@
  */
 
 int print_int(int num){
-    char reversed_str[10] = "", number_str[10] = "";
+    char reversed_str[15] = "", number_str[15] = "";
     char dummy_str[2] = "D";
     int is_negative = 0, i, len = 0;
 
-    
     if (num < 0){
         is_negative = 1;
-        num *= -1;
+        num = -num;
+        
     }
-
     
     while ((num / 10) > 0)
         {
-            
             dummy_str[0] = (num % 10) + '0';
             strcat(reversed_str, dummy_str);
             num /= 10;
         }
+    
+    if (num == INT_MIN){
+        num = INT_MAX;
+
+        while ((num / 10) > 0)
+            {
+                dummy_str[0] = (num % 10) + '0';
+                if (strlen(reversed_str) == 0){
+                    dummy_str[0] = (num % 10) + 1 + '0';
+                }
+                strcat(reversed_str, dummy_str);
+                num /= 10;
+            }
+    }
 
     
     dummy_str[0] = num + '0';
